981589b2051b3ad745adb817710af73410a96b28
Isaiah Plascencia <60566856+zaived@users.noreply.github.com>
2022-02-24 12:20:07 -0800

    WASDW

--- comp/Header.js
+++ comp/Header.js
@@ -3,24 +3,26 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="h-container">
-      <header>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>
-                <h1 className="h-name">Scarlett Gray</h1>
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </header>
-      <Clock
-        className="clock"
-        format={"h:mm:ssa"}
-        style={{ fontSize: "50px" }}
-        ticking={true}
-      />
+    <div className="h-wrapper">
+      <div className="h-container">
+        <header>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>
+                  <h1 className="h-name">Scarlett Gray</h1>
+                </a>
+              </Link>
+            </li>
+          </ul>
+        </header>
+        <Clock
+          className="clock"
+          format={"h:mm:ssa"}
+          style={{ fontSize: "50px" }}
+          ticking={true}
+        />
+      </div>
     </div>
   );
 };
