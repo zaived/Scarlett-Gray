f1f5625fd4d8c6c40bc3b2ae4f361995fd832c0f
Isaiah Plascencia <60566856+zaived@users.noreply.github.com>
2022-03-21 21:51:13 -0700

    WASD

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="container"></div>
+      <div className="home-container">
+        <h2>
+          “At first, art imitates life. Then life will imitate art. Then life
+          will find its very existence from the arts." - Fyodor Dostoevsky
+        </h2>
+      </div>
     </div>
   );
 }
