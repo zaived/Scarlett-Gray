59b0c8db3958296e4c7b813a7ca5df7e985772c2
olivierpoupier <olivier@poupier.net>
2022-08-22 20:01:54 -0400

    fix overlap problems

--- comp/Footer.js
+++ comp/Footer.js
@@ -3,14 +3,14 @@ const Footer = () => {
     <div className="f-container">
       <div className="scroll-right">
         <p>
-          Creative Director - Artist - Hockey Player Creative Director - Artist
-          - Hockey Player Creative Director - Artist - Hockey Player Creative
-          Director - Artist - Hockey Player Creative Director - Artist - Hockey
-          Player Creative Director - Artist - Hockey Player Creative Director -
-          Artist - Hockey Player Creative Director - Artist - Hockey Player
-          Creative Director - Artist - Hockey Player Creative Director - Artist
-          - Hockey Player Creative Director - Artist - Hockey Player Creative
-          Director - Artist - Hockey Player
+          Creative Director - Artist - Hockey Player - Creative Director - Artist
+          - Hockey Player - Creative Director - Artist - Hockey Player - Creative
+          Director - Artist - Hockey Player - Creative Director - Artist - Hockey
+          Player - Creative Director - Artist - Hockey Player - Creative Director -
+          Artist - Hockey Player - Creative Director - Artist - Hockey Player -
+          Creative Director - Artist - Hockey Player - Creative Director - Artist
+          - Hockey Player Creative Director - Artist - Hockey Player - Creative
+          Director - Artist - Hockey Player -
         </p>
       </div>
     </div>
